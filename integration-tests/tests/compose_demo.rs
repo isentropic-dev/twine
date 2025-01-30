a639b6499da340a3242e46c11f0991d77d2f2e61
John Dyreby <john@isentropic.dev>
2025-01-29 21:34:56 -0600

    Start with parsing

--- integration-tests/tests/compose_demo.rs
+++ integration-tests/tests/compose_demo.rs
@@ -1,93 +1,41 @@
-use integration_tests::test_components::{building, hourly_weather};
+#![allow(dead_code)]
+
+// use integration_tests::test_components::{building::BuildingModel, hourly_weather::HourlyWeather};
 use twine_core::compose;
 
-compose!(demo, {
-    Input {
-        time: f64,
-        indoor: {
-            occupancy: u32,
-            pressure: f64,
-            temp_setpoint: f64,
-        },
-        thermostat_control: {
-            is_auto: bool,
-        },
-    }
+struct Demo;
 
-    weather => hourly_weather {
-        time,
-    }
+struct DemoInput {
+    occupancy: u32,
+    temp_setpoint: f64,
+    time: f64,
+}
 
-    first_house => building {
-        occupancy: indoor.occupancy,
-        outdoor_temp: weather.temperature,
-        wind_speed: weather.wind_speed,
-        thermostat: building::Thermostat {
-            setpoint: indoor.temp_setpoint,
-            auto: thermostat_control.is_auto,
-        },
-    }
+#[compose]
+impl Demo {
+    type Input = DemoInput;
 
-    second_house => building {
-        occupancy: indoor.occupancy,
-        outdoor_temp: first_house.indoor_temp,
-        wind_speed: 0.0,
-        thermostat: building::Thermostat {
-            setpoint: 20.0,
-            auto: false,
-        },
-    }
-});
+    fn components() {
+        let weather = HourlyWeather { time: input.time };
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+        let first_house = BuildingModel {
+            occupancy: input.occupancy,
+            outdoor_temp: weather.temperature,
+            wind_speed: weather.wind_speed,
+            thermostat: building::Thermostat {
+                setpoint: input.sure,
+                auto: true,
+            },
+        };
 
-    /// Serializes and prints the given struct in JSON, TOML, and YAML formats.
-    fn print_serialized<T: serde::Serialize + std::fmt::Debug>(label: &str, value: &T) {
-        println!("\n==================== {label} ====================");
-        println!("{value:#?}");
-
-        println!("---------------------- JSON ----------------------");
-        println!("{}", serde_json::to_string_pretty(value).unwrap());
-
-        println!("---------------------- TOML ----------------------");
-        println!("{}", toml::to_string(value).unwrap());
-
-        println!("---------------------- YAML ----------------------");
-        println!("{}", serde_yaml::to_string(value).unwrap());
-
-        println!("=================================================\n");
-    }
-
-    #[test]
-    fn inspect_demo_component() {
-        let demo_config = demo::Config::default();
-        let demo_input = demo::Input::default();
-        let demo_output = demo::Output::default();
-
-        print_serialized("Config", &demo_config);
-        print_serialized("Input", &demo_input);
-        print_serialized("Output", &demo_output);
-
-        assert!(
-            serde_json::to_string(&demo_config).is_ok(),
-            "Config JSON serialization failed."
-        );
-        assert!(
-            serde_json::to_string(&demo_input).is_ok(),
-            "Input JSON serialization failed."
-        );
-        assert!(
-            serde_json::to_string(&demo_output).is_ok(),
-            "Output JSON serialization failed."
-        );
-    }
-
-    #[test]
-    fn call_demo_component() {
-        let demo_fn = demo::create(demo::Config::default());
-        let output = demo_fn(demo::Input::default());
-        println!("{output:#?}");
+        let second_house = BuildingModel {
+            occupancy: input.occupancy,
+            outdoor_temp: first_house.indoor_temp,
+            wind_speed: 0.0,
+            thermostat: building::Thermostat {
+                setpoint: 20.0,
+                auto: false,
+            },
+        };
     }
 }
--- twine-macros/src/compose.rs
+++ twine-macros/src/compose.rs
@@ -1,8 +1,8 @@
+#![allow(dead_code)] // Just for now...
+
 mod generate;
 mod graph;
 mod parse;
-
-use std::collections::HashMap;
 
 use petgraph::{
     algo::toposort,
@@ -10,25 +10,26 @@ use petgraph::{
     Direction,
 };
 use proc_macro::TokenStream;
-use syn::{parse_macro_input, ExprStruct, Ident, Path, Type};
+use syn::{parse_macro_input, ExprStruct, Ident, Path};
 
-pub(crate) fn expand(input: TokenStream) -> TokenStream {
-    let definition = parse_macro_input!(input as ComponentDefinition);
+pub(crate) fn expand(_attr: TokenStream, item: TokenStream) -> TokenStream {
+    let definition = parse_macro_input!(item as ComponentDefinition);
     let graph = definition.into();
-    generate::generate_module(&graph).into()
+    generate::code(&graph).into()
 }
 
 /// Defines a composed component.
-#[derive(Debug)]
 struct ComponentDefinition {
-    /// Component name.
-    name: Ident,
-
-    /// Schema defining the `Input` type.
-    input_schema: InputSchema,
-
-    /// Inner component instances.
+    component_type: Path,
+    input_type: Path,
     components: Vec<ComponentInstance>,
+}
+
+/// Represents an instance of an inner component.
+struct ComponentInstance {
+    name: Ident,
+    component_type: Path,
+    input_expr: ExprStruct,
 }
 
 struct ComponentGraph {
@@ -44,24 +45,6 @@ impl From<ComponentDefinition> for ComponentGraph {
             dependencies,
         }
     }
-}
-
-/// Maps field identifiers to their corresponding input types or structures.
-type InputSchema = HashMap<Ident, InputField>;
-
-/// Represents an input field as a type or nested schema.
-#[derive(Debug)]
-enum InputField {
-    Type(Type),
-    Struct(InputSchema),
-}
-
-/// Represents an instance of an inner component.
-#[derive(Debug)]
-struct ComponentInstance {
-    name: Ident,
-    module: Path,
-    input_struct: ExprStruct,
 }
 
 impl ComponentGraph {
