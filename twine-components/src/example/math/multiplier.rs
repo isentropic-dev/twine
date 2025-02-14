42976a6db01fccb715db142058309670cab4f76e
John Dyreby <john@isentropic.dev>
2025-02-14 10:55:26 -0600

    Add a few example components that do math

---
+++ twine-components/src/example/math/multiplier.rs
@@ -0,0 +1,41 @@
+use std::ops::Mul;
+
+use twine_core::Component;
+
+/// A component that multiplies an input value by a constant factor.
+///
+/// Given an input of type `T`, this component multiplies it by a predefined
+/// factor and produces an output of the same type.
+///
+/// # Type Parameters
+///
+/// - `T`: The input and output type, which must support multiplication.
+pub struct Multiplier<T> {
+    factor: T,
+}
+
+impl<T> Multiplier<T>
+where
+    T: Mul<Output = T> + Copy,
+{
+    /// Creates a new [`Multiplier`] with the specified factor.
+    ///
+    /// # Parameters
+    ///
+    /// - `factor`: The value by which each input will be multiplied.
+    pub fn new(factor: T) -> Self {
+        Self { factor }
+    }
+}
+
+impl<T> Component for Multiplier<T>
+where
+    T: Mul<Output = T> + Copy,
+{
+    type Input = T;
+    type Output = T;
+
+    fn call(&self, input: Self::Input) -> Self::Output {
+        input * self.factor
+    }
+}
