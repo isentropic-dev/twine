b6b388673fc844b034db1ce3009a2b96ee52da25
John Dyreby <john@isentropic.dev>
2025-04-02 13:33:54 -0500

    Add twine-plot with minimal functionality

---
+++ twine-plot/src/lib.rs
@@ -0,0 +1,56 @@
+use eframe::egui;
+use egui_plot::{Legend, Line, Plot, PlotPoint};
+
+/// A runnable egui application for plotting data.
+#[derive(Default)]
+pub struct PlotApp {
+    series: Vec<Series>,
+}
+
+struct Series {
+    label: String,
+    points: Vec<PlotPoint>,
+}
+
+impl PlotApp {
+    #[must_use]
+    pub fn new() -> Self {
+        Self::default()
+    }
+
+    #[must_use]
+    pub fn add_series(mut self, label: &str, points: &[[f64; 2]]) -> Self {
+        self.series.push(Series {
+            label: label.to_string(),
+            points: points.iter().copied().map(Into::into).collect(),
+        });
+
+        self
+    }
+
+    #[allow(clippy::missing_errors_doc)]
+    pub fn run(self, name: &str) -> Result<(), eframe::Error> {
+        eframe::run_native(
+            name,
+            eframe::NativeOptions::default(),
+            Box::new(|_cc| Ok(Box::new(self))),
+        )
+    }
+}
+
+impl eframe::App for PlotApp {
+    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+        egui::CentralPanel::default().show(ctx, |ui| {
+            Plot::new("plot-id")
+                .legend(Legend::default())
+                .show(ui, |plot_ui| {
+                    for series in &self.series {
+                        let points = series.points.as_slice();
+                        let name = &series.label;
+
+                        plot_ui.line(Line::new(points).name(name));
+                    }
+                });
+        });
+    }
+}
