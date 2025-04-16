10b8b1733155047bcdc208206c1cc6232b467afa
Greg Troszak <greg@gregtroszak.me>
2025-04-16 11:08:00 -0400

    Adds the ND interpolation component (#87)

--- twine-components/src/interpolation.rs
+++ twine-components/src/interpolation.rs
@@ -1,10 +1,12 @@
 pub mod error;
 pub mod extrapolate;
+pub mod n;
 pub mod one;
 pub mod three;
 pub mod two;
 
 pub use extrapolate::Extrapolate;
+pub use n::{InterpND, StrategyND};
 pub use one::{Interp1D, Strategy1D};
 pub use three::{Interp3D, Strategy3D};
 pub use two::{Interp2D, Strategy2D};
