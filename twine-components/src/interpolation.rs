51701985a39fa67ee944dafcea761535e2c35e5d
Greg Troszak <greg@gregtroszak.me>
2025-04-16 11:04:43 -0400

    Adds the 3D interpolation component (#86)

--- twine-components/src/interpolation.rs
+++ twine-components/src/interpolation.rs
@@ -1,8 +1,10 @@
 pub mod error;
 pub mod extrapolate;
 pub mod one;
+pub mod three;
 pub mod two;
 
 pub use extrapolate::Extrapolate;
 pub use one::{Interp1D, Strategy1D};
+pub use three::{Interp3D, Strategy3D};
 pub use two::{Interp2D, Strategy2D};
