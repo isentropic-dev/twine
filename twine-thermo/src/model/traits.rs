e192a1ea90420952fb1396b298aa4339ba624f0e
John Dyreby <john@isentropic.dev>
2025-07-12 11:31:53 -0500

    And here

--- twine-thermo/src/model/traits.rs
+++ twine-thermo/src/model/traits.rs
@@ -23,48 +23,51 @@ use crate::{
 ///
 /// This approach supports both broad reuse and precise specialization,
 /// depending on the modeling context.
-pub trait ThermodynamicProperties<F> {
+pub trait ThermodynamicProperties<Fluid> {
     /// Returns the pressure for the given state.
     ///
     /// # Errors
     ///
     /// Returns a [`PropertyError`] if the pressure cannot be calculated.
-    fn pressure(&self, state: &State<F>) -> Result<Pressure, PropertyError>;
+    fn pressure(&self, state: &State<Fluid>) -> Result<Pressure, PropertyError>;
 
     /// Returns the specific internal energy for the given state.
     ///
     /// # Errors
     ///
     /// Returns a [`PropertyError`] if the internal energy cannot be calculated.
-    fn internal_energy(&self, state: &State<F>) -> Result<SpecificInternalEnergy, PropertyError>;
+    fn internal_energy(
+        &self,
+        state: &State<Fluid>,
+    ) -> Result<SpecificInternalEnergy, PropertyError>;
 
     /// Returns the specific enthalpy for the given state.
     ///
     /// # Errors
     ///
     /// Returns a [`PropertyError`] if the enthalpy cannot be calculated.
-    fn enthalpy(&self, state: &State<F>) -> Result<SpecificEnthalpy, PropertyError>;
+    fn enthalpy(&self, state: &State<Fluid>) -> Result<SpecificEnthalpy, PropertyError>;
 
     /// Returns the specific entropy for the given state.
     ///
     /// # Errors
     ///
     /// Returns a [`PropertyError`] if the entropy cannot be calculated.
-    fn entropy(&self, state: &State<F>) -> Result<SpecificEntropy, PropertyError>;
+    fn entropy(&self, state: &State<Fluid>) -> Result<SpecificEntropy, PropertyError>;
 
     /// Returns the specific heat capacity at constant pressure.
     ///
     /// # Errors
     ///
     /// Returns a [`PropertyError`] if `cp` cannot be calculated.
-    fn cp(&self, state: &State<F>) -> Result<SpecificHeatCapacity, PropertyError>;
+    fn cp(&self, state: &State<Fluid>) -> Result<SpecificHeatCapacity, PropertyError>;
 
     /// Returns the specific heat capacity at constant volume.
     ///
     /// # Errors
     ///
     /// Returns a [`PropertyError`] if `cv` cannot be calculated.
-    fn cv(&self, state: &State<F>) -> Result<SpecificHeatCapacity, PropertyError>;
+    fn cv(&self, state: &State<Fluid>) -> Result<SpecificHeatCapacity, PropertyError>;
 }
 
 /// Trait for creating thermodynamic states from various input combinations.
