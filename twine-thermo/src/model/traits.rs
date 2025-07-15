9365e7016d5852bb17ba624e3fe62000bdbddddf
John Dyreby <john@isentropic.dev>
2025-07-15 14:30:26 -0500

    Scaffold the traits

--- twine-thermo/src/model/traits.rs
+++ twine-thermo/src/model/traits.rs
@@ -1,5 +1,9 @@
+mod flow_operations;
 mod state_from;
+mod state_operations;
 mod thermodynamic_properties;
 
+pub use flow_operations::FlowOperations;
 pub use state_from::StateFrom;
+pub use state_operations::StateOperations;
 pub use thermodynamic_properties::ThermodynamicProperties;
