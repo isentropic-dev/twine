a97f6f2ee6b1ef07441e352cbb7d8ff613b4941f
Greg Troszak <greg@isentropic.dev>
2025-10-08 10:03:03 -0400

    Adds effectiveness-NTU relations for shell-and-tube (#183)

--- twine-components/src/thermal/hx/arrangement.rs
+++ twine-components/src/thermal/hx/arrangement.rs
@@ -3,7 +3,9 @@
 mod counter_flow;
 mod cross_flow;
 mod parallel_flow;
+mod shell_and_tube;
 
 pub use counter_flow::CounterFlow;
 pub use cross_flow::{CrossFlow, Mixed, Unmixed};
 pub use parallel_flow::ParallelFlow;
+pub use shell_and_tube::ShellAndTube;
