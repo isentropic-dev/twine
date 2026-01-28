0e74255cb6ec019fac696b63c18576e61359fc04
John Dyreby <john@isentropic.dev>
2026-01-28 13:12:18 -0600

    twine-solve: promote Observer to crate root

--- twine-solve/src/equation.rs
+++ twine-solve/src/equation.rs
@@ -1,9 +1,7 @@
 mod evaluate;
-mod observe;
 mod problem;
 
 pub mod bisection;
 
 pub use evaluate::{EvalError, EvaluateResult, Evaluation, evaluate};
-pub use observe::Observer;
 pub use problem::EquationProblem;
