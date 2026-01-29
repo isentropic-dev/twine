95efb0bef3f6d11aa08f9b370121dbd1e0fbcd66
John Dyreby <john@isentropic.dev>
2026-01-29 10:43:21 -0600

    twine-solve: add optimization evaluate module (#222)

--- twine-solve/src/optimization.rs
+++ twine-solve/src/optimization.rs
@@ -1,5 +1,7 @@
+mod evaluate;
 mod goal;
 mod problem;
 
+pub use evaluate::{EvalError, EvaluateResult, Evaluation, evaluate};
 pub use goal::{Goal, Maximize, Minimize};
 pub use problem::OptimizationProblem;
