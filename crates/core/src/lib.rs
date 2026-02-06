b898fbc7fabcadc4b86f075f67e1a69d667cf470
John Dyreby <john@isentropic.dev>
2026-02-06 16:47:31 -0600

    core: add StepIntegrable and OdeProblem traits

--- crates/core/src/lib.rs
+++ crates/core/src/lib.rs
@@ -1,7 +1,9 @@
 mod model;
 mod observer;
 mod problems;
+mod step;
 
 pub use observer::Observer;
-pub use problems::{EquationProblem, MaximizationProblem, MinimizationProblem};
+pub use problems::{EquationProblem, MaximizationProblem, MinimizationProblem, OdeProblem};
+pub use step::{DerivativeOf, StepIntegrable};
 pub use {model::Model, model::Snapshot};
